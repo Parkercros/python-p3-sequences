bd90e90eb9a832a32bcacd6e34528d9bfcecf925
Parker Crosgrove <pdxparker3@gmail.com>
2023-03-14 14:51:50 -0600

    finished

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    fib_list = [0, 1] 
+    for i in range(2, length):
+        next_num = fib_list[i-1] + fib_list[i-2] 
+        fib_list.append(next_num)
+    print(fib_list)
